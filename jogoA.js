518e52ea34265105c13f6ad983f9843fa3fcad00
dafissa <rayssa.9258@colegioprogresso.g12.br>
2025-06-12 09:52:20 -0300

    css

--- jogoA.js
+++ jogoA.js
@@ -5,4 +5,34 @@ class game
 
     this.width = this.canvas.width;
     this.height = this.canvas.height;
+
+    this.brickRowCount = 2;
+    this.brickColumnCount = 6;
+    this.brickWidth = 75;
+    this.brickHeight = 20;
+    this.brickPadding = 10;
+    this.brickOffsetTop = 30;
+    this.brickOffsetLeft = 30;
+    this.brick = [];
+
+    this.level = 1;
+
+    this.baseSpeed = 1.5;
+    this.ballRadius = 10;
+    this.ballDX = this.baseSpeed;
+    this.ballDY = -this.baseSpeed;
+    this.ballX = this.width / 2;
+    this.ballY = this.height - 30;
+
+    this.paddleHeight = 10;
+    this.paddleWidth = 100;
+    this.paddlex = (this.Width - this.paddleWidth) / 2;
+    this.rightPressed = false;
+    this.leftPressed = false;
+    
+    this.score = 0;
+    this.totalBricks = 0;
+    this.highScore = parseInt(localStorage.getItem("arkanoidHighScore")) || 0;
+
+    
     }
